26f292c1452c4839437bbe2780e875c2dc090662
Twinkle_p <twinklepatil869@gmail.com>
2023-07-28 17:27:53 +0530

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'', redirectTo:'home', pathMatch:'full' 
+  },
+  {
+    path:'home',  component:HomeComponent
+  },
+  {
+    path:'header',  component:HeaderComponent
+  },
+  {
+    path:'footer', component:FooterComponent 
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
